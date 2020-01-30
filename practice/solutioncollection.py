97918d11f325c521e85a260e639eaae4947280e8
Wang Yao <wyao@nec-labs.com>
2020-01-30 00:49:58 -0500

    minor update

--- practice/solutioncollection.py
+++ practice/solutioncollection.py
@@ -1,21 +1,23 @@
 class SolutionCollection:
-    """Personel practice on python
+    """
+    Personel practice on python
 
     """
 
-    """ Two Sum
+    """
+    Two Sum
     Problem link:
         https://leetcode.com/problems/two-sum/
 
     Difficulty(Easy, Medium, Hard): Easy
 
     Description:
-        Given an array of integers, return indices of the two numbers such 
-        that 
+        Given an array of integers, return indices of the two numbers such
+        that
         they add up to a specific target.
 
-        You may assume that each input would have exactly one solution, 
-        and you 
+        You may assume that each input would have exactly one solution,
+        and you
         may not use the same element twice.
 
         Example:
@@ -30,22 +32,27 @@ class SolutionCollection:
 
     Is new solution?(Yes/No): No
 
-    New solution date(if new solution):        
+    New solution date(if new solution):
     """
 
-    def twoSum(self, num, target):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
+        Parameters
+        ---------------
+        nums: List. List of integers, that each value is dinstinct
+        target: int. Target integer.
 
-        :param num: input array of number
-        :param target: target that sum of two elements from num must equal
-        :return: tuple of indecies of two number in num that their sum is
-        target
+        Returns
+        ----------------
+        List of indices of nums that the sum of two elements equals target.
         """
         mapping = dict()
-        for i in range(len(num)):
-            x = num[i]
-            if target-x in mapping:
-                return [mapping[target-x], i]
-            mapping[x] = i
-            # just for illustration
-            print(mapping)
+
+        for i, e in enumerate(nums):
+            complement = target - e
+            if complement in mapping:
+                j = mapping.get(complement)
+                if j != i:
+                    return [j, i]
+
+            mapping[e] = i
